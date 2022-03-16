985366689ed6bc6892995b91b2507ef9e053331c
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-16 17:31:13 +0100

    es2 corretto

--- es2/asset/main2.js
+++ es2/asset/main2.js
@@ -2,10 +2,15 @@
 // il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 
-let Nomi = ["Mario", "Giovanni", "Francesco", "Saverio", "Paolo", "Luca", "Alessandro"] 
+let Nomi = ["Mario", "Giovanni", "Francesco", "Saverio", "Paolo", "Luca", "Alessandro"]
 let Cognomi = ["Rossi", "Verde", "Brambilla", "Esposito", "Franceschini", "Marini", "Paoli"]
 let list;
 
-function generator() {
- document.getElementById("name").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
-}
+
+document.getElementById("name").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name2").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name3").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name4").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name5").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name6").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
+document.getElementById("name7").innerHTML = Nomi[Math.floor(Math.random() * Nomi.length)] + " " + Cognomi[Math.floor(Math.random() * Cognomi.length)];
---
+++ es3/asset/main3.js
@@ -0,0 +1,18 @@
+// Snack3
+// Crea un array di numeri interi e 
+// fai la somma di tutti gli elementi  che sono in posizione dispari
+
+
+let n = [2 , 4 , 7 , 9 , 10 , 12 , 15];
+
+let somma = n + i;
+
+totale = somma;
+
+
+for (let i = 1; i < n.length; i = i + 2 ) {
+
+ document.getElementById("stamp").innerHTML += `${totale}`
+  
+}
+
